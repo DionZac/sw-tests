7d76687bfade98fb69ab220ad79c12aad3142f83
DionZac <zacdion333@gmail.com>
2020-11-19 23:16:03 +0200

    =add node server

---
+++ server.js
@@ -0,0 +1,12 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.sendFile('index.html')
+}).listen(8000);
