eb53778cc000e5508fc70d6744075bd0bb72c899
DionZac <zacdion333@gmail.com>
2020-11-19 23:35:02 +0200

    Fix node js server

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('./key.pem', 'utf-8'),
+  cert: fs.readFileSync('./server.crt', 'utf-8')
 };
-
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.sendFile('index.html')
+fs.readFile('./index.html', function(err,html){
+    https.createServer(options, function (req, res) {
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write(html);
+        res.end();
+    })
 }).listen(8000);
